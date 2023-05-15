57be1797e836e1b69a2a4d3a5d27f9c44c43830f
hallestar <wongzicai@gmail.com>
2023-05-15 20:18:04 +0800

    find reference under cursor

--- mappings.lua
+++ mappings.lua
@@ -24,15 +24,16 @@ return {
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
     ["<leader>fx"] = { "<cmd>Telescope nvgtags find_definition<cr>", desc = "Find definition" },
-    ["<leader>fz"] = { "<cmd>Telescope nvgtags find_definition_under_cursor<cr>", desc = "Find definition under corsur" },
+    ["<leader>fz"] = { "<cmd>Telescope nvgtags find_definition_under_cursor<cr>", desc = "Find definition under cursor" },
     ["<leader>fp"] = { "<cmd>Telescope nvgtags find_reference<cr>", desc = "Find reference" },
+    ["<leader>fq"] = { "<cmd>Telescope nvgtags find_reference_under_cursor<cr>", desc = "Find reference under cursor" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-    ["<S-Insert>"] = { "<C-R>+", desc = "shift insert " },  -- paste
+    ["<S-Insert>"] = { "<C-R>+", desc = "shift insert " }, -- paste
     -- map! k<S-Insert> <C-R>+
   },
   c = {
-    ["<S-Insert>"] = { "<C-R>+", desc = "shift insert " },  -- paste
+    ["<S-Insert>"] = { "<C-R>+", desc = "shift insert " }, -- paste
   },
   t = {
     -- setting a mapping to false will disable it
